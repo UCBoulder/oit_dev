d3b08de8d8dcca26a318477730a1bb91c5164053
France <miles.france@gmail.com>
2022-03-03 05:35:12 +0000

    github: use internal drupal directory

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -3,12 +3,6 @@
 # Fail on error
 set -e
 
-echo "--------- Directory looking /root/drupal/vendor/bin -------------"
-ls -al /root/drupal/vendor/bin
-echo "--------- /Directory looking -------------"
-composer validate
-composer global require drupal/coder
-composer global require overtrue/phplint
-export PATH="$PATH:$HOME/.config/composer/vendor/bin"
+export PATH="$PATH:/root/drupal/vendor/bin"
 echo "--------- CS O'Clock -------------"
 phpcs --standard=Drupal,DrupalPractice ./
