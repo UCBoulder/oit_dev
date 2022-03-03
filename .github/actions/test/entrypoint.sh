7e782d4bc7a61e25416f9f6c8ba7e010441b22c6
France <miles.france@gmail.com>
2022-03-03 00:58:55 +0000

    github: manual global install phpcs and attempt to export

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -3,6 +3,6 @@
 # Fail on error
 set -e
 
-composer global show -P
-ls ~/
-vendor/bin/phpcs --standard=Drupal,DrupalPractice ./
+composer global require drupal/coder
+export PATH="$PATH:$HOME/.config/composer/vendor/bin"
+phpcs --standard=Drupal,DrupalPractice ./
