1450dbd82edf16188c50acbf716df1b2bf6acbc0
France <miles.france@gmail.com>
2022-03-03 01:13:37 +0000

    github: linting removed

--- .github/actions/test/entrypoint.sh
+++ .github/actions/test/entrypoint.sh
@@ -6,7 +6,5 @@ set -e
 composer global require drupal/coder
 composer global require overtrue/phplint
 export PATH="$PATH:$HOME/.config/composer/vendor/bin"
-echo "--------- Lint O'Clock -------------"
-phplint --no-cache -v ./
 echo "--------- CS O'Clock -------------"
 phpcs --standard=Drupal,DrupalPractice ./
