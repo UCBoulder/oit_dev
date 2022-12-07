9e4a51118836810e97651e95a39475948f700eb6
protitude <miles.france@gmail.com>
2022-12-07 22:07:01 +0000

    github: switch to reusable workflows

--- .github/actions/test/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Fail on error
-set -e
-
-export PATH="$PATH:/root/drupal/vendor/bin"
-echo "--------- CS O'Clock -------------"
-phpcs --standard=Drupal,DrupalPractice ./
-phpstan analyse -l 3 ./
